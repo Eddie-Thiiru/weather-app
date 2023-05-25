83d6513ae0000e6a9889013136ab3f45a4cdef24
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-05-25 11:43:41 +0300

    Add webpack and webpack.config.js

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+/** *** */ (() => {
+  // webpackBootstrap
+  const __webpack_exports__ = {};
+  /*! **********************!*\
+  !*** ./src/index.js ***!
+  \********************* */
+  function component() {}
+  component();
+
+  /** *** */
+})();
+// # sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWF0aGVyLWFwcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBjb21wb25lbnQoKSB7fVxuY29tcG9uZW50KCk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
